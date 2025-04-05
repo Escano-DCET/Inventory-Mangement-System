daa079327cb536ee7efe78954795ae65ae253021
Ferdinand C. Escaño, Jr. <jrescano8@gmail.com>
2025-04-05 18:26:09 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+//Inventory Management System Functionallities
+//Display table with exisiting products
+//Search product
+//Add product
+//Edit product
+//Delete product
+//Upload image
+//Open add modal
+//Open edit modal
+//Update table
+//Success/error message
